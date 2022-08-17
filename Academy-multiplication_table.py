1df7251737bbfb31e69fe658d06b27b51656e1ce
Masirou <key070703@gmail.com>
2022-08-17 21:23:30 +0900

    Academy-multiplication_table.py Frost Cat - SBS_Academy_Python

--- Academy-multiplication_table.py
+++ Academy-multiplication_table.py
@@ -1,5 +1,4 @@
 # 단을 입력받고 단에 대한 구구단을 제공.
 Inum = int(input("Enter you want num : "))
-for Anum in range(1, 50):
+for Anum in range(1, 10):
     print(f"{Inum} X {Anum} = {Inum*Anum}")
-# git Hub test
