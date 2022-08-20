2c3a333c1eafeb2f5ec34efeaa4caa0594bc21db
Masirou <Key070703@gmail.com>
2022-08-20 13:31:26 +0900

    Main Academy-num_basebal.py

--- Academy-num_baseball.py
+++ Academy-num_baseball.py
@@ -1,6 +1,6 @@
-#입력한 값과 Answer에 있는 자리와 수가 같을땐 strike를 증가,
-#입력한 값이 Answer안에 있는데 자리가 다른땐 Out을 증가
-#아무것도 같지 않으면 아무것도 증가시키지 않음.
+# 입력한 값과 Answer에 있는 자리와 수가 같을땐 strike를 증가,
+# 입력한 값이 Answer안에 있는데 자리가 다른땐 Out을 증가
+# 아무것도 같지 않으면 아무것도 증가시키지 않음.
 
 Answer = "123"
 
@@ -11,10 +11,9 @@ Out = 0
 for i in range(0, len(Answer)):
     if Answer[i] == Base[i]:
         Strike = Strike+1
-    else:
-        if Base[i] in Answer:
-            Out = Out+1
-    #else:
+    elif Base[i] in Answer:
+        Out = Out+1
+    # else:
     #    for a in range(0, len(Answer)):
     #        if Base[i] == Answer[a]:
     #            Out = Out+1
