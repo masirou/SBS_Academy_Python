bc0246b54ce18d208f4dbfe433ad2b1d75ea103e
Masirou <Key070703@gmail.com>
2022-08-20 14:28:05 +0900

    Academy-odd_even_nu

--- Academy-odd_even_num.py
+++ Academy-odd_even_num.py
@@ -2,8 +2,8 @@ odd = 0
 even = 0
 for i in range(1, 101):
     if i % 2 == 0:
-        odd = odd + i
-    else:
         even = even + i
+    else:
+        odd = odd + i
 print(f"Odd num : {odd}")
 print(f"Even num : {even}")
