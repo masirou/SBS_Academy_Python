c87b79b37978c7addca869cac4e4e520be24e2a6
Masirou <Key070703@gmail.com>
2022-08-20 12:28:07 +0900

    test

--- Academy-num_game.py
+++ Academy-num_game.py
@@ -1,8 +1,8 @@
 
-#1, 100까지의 랜덤한 수를 Up,Down하면서 맞추는 게임
+# 1, 100까지의 랜덤한 수를 Up,Down하면서 맞추는 게임
 import random
 
-Rnum = random.randint(1,100)
+Rnum = random.randint(1, 101)
 
 while True:
     Inum = int(input("Number : "))
