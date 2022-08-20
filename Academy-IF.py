a8f6a4af25dd267cb43dc25e57589dfc7102cbcf
Masirou <Key070703@gmail.com>
2022-08-20 14:09:33 +0900

    Academy-If

--- Academy-IF.py
+++ Academy-IF.py
@@ -14,18 +14,18 @@ elif Score >= 0:
 print(f"Score is {Score}, Credit is {Credit}")
 
 # 임의의 정수를 입력받은 뒤 해당 값이 3의 배수인지 아닌지를 판별
-Sum = input("enter the Sum : ")
+Sum = int(input("enter the Sum : "))
 
 
-if int(Sum) % 3 == 0:
+if Sum % 3 == 0:
     print(f"{Sum} is a multiple of 3")
 else:
     print(f"{Sum} is not a multiple of 3")
 
 # 임의의 정수 3개를 입력받아 그 중에서 가장 큰 수를 출력
-Num1 = input(f"[1] enter the Num : ")
-Num2 = input(f"[2] enter the Num : ")
-Num3 = input(f"[3] enter the Num : ")
+Num1 = int(input(f"[1] enter the Num : "))
+Num2 = int(input(f"[2] enter the Num : "))
+Num3 = int(input(f"[3] enter the Num : "))
 if Num1 > Num2:
     if Num1 > Num3:
         print(Num1)
