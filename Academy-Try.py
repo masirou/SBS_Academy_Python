08a5ffbe722f22e5abe4fac1adf70372dde54588
Masirou <Key070703@gmail.com>
2022-08-21 14:57:18 +0900

    Academy-Try.py

---
+++ Academy-Try.py
@@ -0,0 +1,12 @@
+
+
+f = open("file.txt", encoding="UTF-8")
+Mylist = list(f.read().split())
+count = {}
+
+for i in Mylist:
+    try:
+        count[i] += 1
+    except:
+        count[i] = 1
+print(count)
